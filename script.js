6a0cd4c34ac1fef35406c668a68e397eed70ba5b
moeenpatel <123477545+moeenpatel@users.noreply.github.com>
2023-09-18 16:26:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const byteSize = (str) => {
   // write your code here
+	const blob = new Blob([str], { type: 'text/plain' });
+
+  // Get the size of the Blob in bytes
+  const sizeInBytes = blob.size;
+
+  return sizeInBytes;
 };
 
 // Do not change the code below
